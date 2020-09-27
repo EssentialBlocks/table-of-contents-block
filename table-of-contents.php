46461def1d30147d2f95f2b525fe19f94367919b
Tasnim Alam <tasnimalamcse@gmail.com>
2020-09-27 09:40:55 +0600

    Update plugin info

--- table-of-contents.php
+++ table-of-contents.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * Plugin Name:     Table Of Contents
- * Description:     Example block written with ESNext standard and JSX support – build step required.
- * Version:         0.1.0
- * Author:          The WordPress Contributors
- * License:         GPL-2.0-or-later
- * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Plugin URI: 		https://essential-blocks.com
+ * Description:     Insert Table of Contents on your posts/pages and enhance user experience on your WordPress website
+ * Version:         1.0.0
+ * Author:          WPDeveloper
+ * Author URI: 		https://wpdeveloper.net
+ * License:         GPL-3.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:     table-of-contents-block
  *
  * @package         table-of-contents-block
