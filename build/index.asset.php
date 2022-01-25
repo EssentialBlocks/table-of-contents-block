be5af3ef67e342dfce9f0e9a2a303a73eecdca65
Rahat Hossain <rahat.feni99@gmail.com>
2022-01-25 13:03:20 +0600

    updated deployment script

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2d105bc9761269e74ead575e42ffd5e6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c810f16af007670577c15a15ee038c7b');
