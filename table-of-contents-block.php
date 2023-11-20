edb166220dfb3b10f50aedeb2b75fded3580635e
Monir <fencemonir@gmail.com>
2023-11-20 15:19:40 +0600

    update version

--- table-of-contents-block.php
+++ table-of-contents-block.php
@@ -2,11 +2,11 @@
 
 /**
  * Plugin Name:     Table Of Contents Block
- * Plugin URI:         https://essential-blocks.com
+ * Plugin URI:      https://essential-blocks.com
  * Description:     Automatically Add Table of Contents Block for your WordPress Posts & Pages
  * Version:         1.3.5
  * Author:          WPDeveloper
- * Author URI:         https://wpdeveloper.net
+ * Author URI:     	https://wpdeveloper.net
  * License:         GPL-3.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:     table-of-contents-block
